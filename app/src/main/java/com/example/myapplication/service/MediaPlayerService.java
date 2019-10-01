75574a7db3848078d72b23eb4378b0591af347ea
Anica <developer.anica@gmail.com>
2019-10-01 15:12:21 +0200

    init commit

--- app/src/main/java/com/example/myapplication/service/MediaPlayerService.java
+++ app/src/main/java/com/example/myapplication/service/MediaPlayerService.java
@@ -4,7 +4,6 @@ import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.IBinder;
-
 import androidx.annotation.Nullable;
 
 public class MediaPlayerService extends Service implements
@@ -32,4 +31,62 @@ public class MediaPlayerService extends Service implements
     public void onPrepared(MediaPlayer mediaPlayer) {
 
     }
+
+    private void playTrack(){
+
+    }
+
+    private void pauseTrack(){
+
+    }
+
+    private void nextTrack(){
+
+    }
+
+    private void previousTrack(){
+
+    }
+
+    private void repeatTrack(){
+
+    }
+
+    private void repeatAll(){
+
+    }
+
+    /*private void getFirstTrack(final int id, final ArrayList<Track> array, final Context context) {
+        if(!array.isEmpty()){
+            try {
+                play.setImageDrawable(getResources().getDrawable(R.drawable.ic_play));
+                playBottom.setImageDrawable(getResources().getDrawable(R.drawable.ic_play));
+                Track track;
+                // Retrieve id from somewhere
+                track = array.get(id);
+                long l = track.getId();
+                songName.setText(track.getTrackName());
+                songName_bottom.setText(track.getTrackName());
+                artistName_bottom.setText(track.getArtistName());
+                trackDuration_bottom.setText(track.getTrackDuration());
+                // Get track info
+                editor = sharedPreferences.edit();
+                editor.putString("trackName", track.getTrackName());
+                editor.putString("trackArtist", track.getArtistName());
+                editor.putString("trackDuration", track.getTrackDuration());
+                editor.apply();
+                // Return to idle state
+                player.reset();
+                // Here the player object we earlier created is initialized once we call setDataSource.
+                player.setDataSource(context, ContentUris.withAppendedId
+                        (android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, l));
+                player.prepare();
+                start();
+            } catch (IOException e) {
+                Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }else {
+            Toast.makeText(getApplicationContext(), "Add music to begin", Toast.LENGTH_SHORT).show();
+        }
+    }*/
 }
